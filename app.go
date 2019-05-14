53964f5d055e3e34ab5c72c9acca770f321ea321
Planxnx <60010465@kmitl.ac.th>
2019-05-14 20:10:24 +0700

    change code

--- app.go
+++ app.go
@@ -1,132 +1,31 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
-
-	"github.com/gorilla/mux"
 )
-
-const (
-	FACEBOOK_API = "https://graph.facebook.com/v3.3/me/messages?access_token=EAAf5m6IkbZAUBAGfEZALvFtyRj8ahcT0kE9FTKiCz7owzkdwthWDabMHrfhZCEhouIj9pzbYJBnbbWzhZBycU8QVLGyBoxqzflv7AgLQLAD7m0zSCtLdjz1nWdB1JZAiFYMzbpPZCfdZAoUAYKgHGhizfD8j71cgTrlaOdj4EYBZBtHeZBQZCtC6XU"
-	IMAGE        = "http://37.media.tumblr.com/e705e901302b5925ffb2bcf3cacb5bcd/tumblr_n6vxziSQD11slv6upo3_500.gif"
-)
-
-type Callback struct {
-	Object string `json:"object,omitempty"`
-	Entry  []struct {
-		ID        string      `json:"id,omitempty"`
-		Time      int         `json:"time,omitempty"`
-		Messaging []Messaging `json:"messaging,omitempty"`
-	} `json:"entry,omitempty"`
-}
-
-type Messaging struct {
-	Sender    User    `json:"sender,omitempty"`
-	Recipient User    `json:"recipient,omitempty"`
-	Timestamp int     `json:"timestamp,omitempty"`
-	Message   Message `json:"message,omitempty"`
-}
-
-type User struct {
-	ID string `json:"id,omitempty"`
-}
-
-type Message struct {
-	MID        string `json:"mid,omitempty"`
-	Text       string `json:"text,omitempty"`
-	QuickReply *struct {
-		Payload string `json:"payload,omitempty"`
-	} `json:"quick_reply,omitempty"`
-	Attachments *[]Attachment `json:"attachments,omitempty"`
-	Attachment  *Attachment   `json:"attachment,omitempty"`
-}
-
-type Attachment struct {
-	Type    string  `json:"type,omitempty"`
-	Payload Payload `json:"payload,omitempty"`
-}
-
-type Response struct {
-	Recipient User    `json:"recipient,omitempty"`
-	Message   Message `json:"message,omitempty"`
-}
-
-type Payload struct {
-	URL string `json:"url,omitempty"`
-}
-
-func VerificationEndpoint(w http.ResponseWriter, r *http.Request) {
-	challenge := r.URL.Query().Get("hub.challenge")
-	mode := r.URL.Query().Get("hub.mode")
-	token := r.URL.Query().Get("hub.verify_token")
-
-	if mode != "" && token == os.Getenv("golang_chatbot") {
-		w.WriteHeader(200)
-		w.Write([]byte(challenge))
-	} else {
-		w.WriteHeader(404)
-		w.Write([]byte("Error, wrong validation token"))
-	}
-}
-
-func ProcessMessage(event Messaging) {
-	client := &http.Client{}
-	response := Response{
-		Recipient: User{
-			ID: event.Sender.ID,
-		},
-		Message: Message{
-			Attachment: &Attachment{
-				Type: "image",
-				Payload: Payload{
-					URL: IMAGE,
-				},
-			},
-		},
-	}
-	body := new(bytes.Buffer)
-	json.NewEncoder(body).Encode(&response)
-	url := fmt.Sprintf(FACEBOOK_API, os.Getenv("EAAf5m6IkbZAUBAGfEZALvFtyRj8ahcT0kE9FTKiCz7owzkdwthWDabMHrfhZCEhouIj9pzbYJBnbbWzhZBycU8QVLGyBoxqzflv7AgLQLAD7m0zSCtLdjz1nWdB1JZAiFYMzbpPZCfdZAoUAYKgHGhizfD8j71cgTrlaOdj4EYBZBtHeZBQZCtC6XU"))
-	req, err := http.NewRequest("POST", url, body)
-	req.Header.Add("Content-Type", "application/json")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-}
-
-func MessagesEndpoint(w http.ResponseWriter, r *http.Request) {
-	var callback Callback
-	json.NewDecoder(r.Body).Decode(&callback)
-	if callback.Object == "page" {
-		for _, entry := range callback.Entry {
-			for _, event := range entry.Messaging {
-				ProcessMessage(event)
-			}
-		}
-		w.WriteHeader(200)
-		w.Write([]byte("Got your message"))
-	} else {
-		w.WriteHeader(404)
-		w.Write([]byte("Message not supported"))
-	}
-}
 
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/webhook", VerificationEndpoint).Methods("GET")
-	r.HandleFunc("/webhook", MessagesEndpoint).Methods("POST")
-	if err := http.ListenAndServe("0.0.0.0:8080", r); err != nil {
-		log.Fatal(err)
+	http.HandleFunc("/webhook", MessengerVerify)
+
+	fmt.Println("Starting server on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func MessengerVerify(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		challenge := r.URL.Query().Get("hub.challenge")
+		verify_token := r.URL.Query().Get("hub.verify_token")
+
+		if len(verify_token) > 0 && len(challenge) > 0 && verify_token == "planx-golang" {
+			w.Header().Set("Content-Type", "text/plain")
+			fmt.Fprintf(w, challenge)
+			return
+		}
 	}
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(400)
+	fmt.Fprintf(w, "Bad Request")
 }
